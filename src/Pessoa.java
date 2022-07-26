90595931019000755e716b45652287b385fccd90
rhanielfarias <rhanielfarias@gmail.com>
2022-07-26 13:01:22 -0300

    Criando classe modelo pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,36 @@
+public abstract class Pessoa {
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Pessoa(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
