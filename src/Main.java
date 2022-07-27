98b18b92639a92b4de611e765375ddec6561ba09
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-27 13:03:30 -0300

    Inserindo atualizações de lógica e de interação, feitas na Main

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,61 @@ public class Main {
     public static void main(String[] args) {
         boolean continuar = true;
         Scanner input = new Scanner(System.in);
-        ControleRegistro vendedor = new ControleRegistro();
-        ControleRegistro vendas = new ControleRegistro();
+        ControleRegistro controleRegistro = new ControleRegistro();
+
+        System.out.println("-----------------------------------------------");
+        System.out.println("Bem-vindo(a) ao menu de registro de vendas.");
+        System.out.println("-----------------------------------------------");
 
         while (continuar) {
 
-                    System.out.println("-------------------------------------");
-                    System.out.println("Bem-vindo(a) ao menu de registro de vendas.");
-                    System.out.println("-------------------------------------");
-                    System.out.println("Digite o número [1] - Para cadastrar uma venda");
-                    System.out.println("Digite o número [2] - Para cadastrar um vendedor");
-                    System.out.println("Digite o número [3] - Para exibir a lista de vendedores cadastrados");
-                    System.out.println("Digite o número [4] - Para sair do menu\n");
+            System.out.println("Digite o número [1] - Para cadastrar um vendedor");
+            System.out.println("Digite o número [2] - Para cadastrar cliente");
+            System.out.println("Digite o número [3] - Para cadastrar a venda");
+            System.out.println("Digite o número [4] - Para exibir os vendedores cadastrados");
+            System.out.println("Digite o número [5] - Para exibir os clientes cadastrados");
+            System.out.println("Digite o número [6] - Para exibir vendas cadastradas");
+            System.out.println("Digite o número [7] - Para sair do sistema de registro\n");
 
-                    System.out.print("Informe um número e execute uma das opções acima: ");
-                    int opcao = input.nextInt();
-                    switch (opcao) {
-                        case 1:
-                            vendas.cadastrarProduto();
-                            break;
-                        case 2:
-                            vendedor.cadastrarVendedor();
+            System.out.print("Informe um número e execute uma das opções apresentadas: ");
+            int opcao = input.nextInt();
+            switch (opcao) {
+                case 1:
+                    controleRegistro.cadastrarVendedor();
+                    break;
+                case 2:
+                    controleRegistro.cadastrarCliente();
+                    break;
+                case 3:
+                    controleRegistro.cadastrarVenda();
+                    break;
+                case 4:
+                    System.out.println("\n----------------------------");
+                    System.out.println("Vendedores cadastrados: ");
+                    System.out.println("----------------------------");
+                    controleRegistro.listarVendedor();
+                    break;
+                case 5:
+                    System.out.println("\n----------------------------");
+                    System.out.println("Clientes cadastrados: ");
+                    System.out.println("----------------------------");
+                    controleRegistro.listarClientes();
+                    break;
 
-                            break;
-                        case 3:
-                            vendedor.listarVendedor();
-                            break;
-                        case 4:
-                            continuar = false;
-                            System.out.println("Ok, saindo do menu de registro de vendas...");
-                            break;
-                        default:
-                            System.out.println("Opção invalida, digite um número das opções acima");
-                    }
+                case 6:
+                    System.out.println("\n----------------------------");
+                    System.out.println("Vendas cadastradas: ");
+                    System.out.println("----------------------------");
+                    controleRegistro.listarVendas();
+                    break;
+                case 7:
+                    continuar = false;
+                    System.out.println("Ok, saindo do sistema de registro... ");
+                    break;
+                default:
+                    System.out.println("Opção invalida, digite um número das opções abaixo \n");
             }
-
         }
+
     }
+}
